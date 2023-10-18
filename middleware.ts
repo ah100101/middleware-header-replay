541b1359a4cea3ad8f2ed300a3d9867a4221cc2a
Alex <alex.hawley@vercel.com>
2023-10-18 13:50:16 -0500

    testing response info

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import { geolocation } from "@vercel/edge";
 
 export const matcher = ["/header"];
 
-export function middleware(request: NextRequest) {
+export function middleware(request: NextRequest, response: Response) {
+  const geoData = geolocation(request);
+  // console.log(JSON.stringify(geoData));
   const header = request.nextUrl.searchParams.get("h") ?? "";
-
   request.headers.set("x-vercel-header-request", header);
+  request.headers.set("x-custom-header-ip", request.ip ?? "");
+
+  const responseHeaders = response.headers;
+  console.log(JSON.stringify({ response }));
+
   return NextResponse.rewrite(request.nextUrl, {
     headers: {
       "x-vercel-header-response": header,
