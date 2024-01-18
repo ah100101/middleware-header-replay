c1468794cbf2273abcc555c606cd3774291526d2
Alex Hawley <alex.hawley@vercel.com>
2024-01-18 16:35:10 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/header',
+        headers: [
+          {
+            key: 'host',
+            value: 'shop.middleware-header-replay.vercel.app',
+          },
+        ],
+      },
+    ]
+  },
+}
 module.exports = nextConfig
